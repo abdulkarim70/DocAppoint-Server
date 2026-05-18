3b36c53f0a2fef53d1a3d6e97cbef126e1a25985
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-05-18 22:38:06 +0600

    footer added

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-const express = require('express')
-const app = express()
-const dotenv=require('dotenv')
-dotenv.config()
-const port = process.env.PORT || 8080
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+dotenv.config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI
+const app = express();
+const port = process.env.PORT || 8080;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   
     await client.connect();
-   
+
+    const db = client.db("docdb");
+    const appointmentCollection = db.collection("doctorapp");
+
+    app.get("/appointments", async (req, res) => {
+      const result = await appointmentCollection.find().toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-  
-    // await client.close();
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
   }
 }
-run().catch(console.dir);
 
-const db= client.db('docdb');
-const appointmentCollection=db.collection('doctorapp')
-
-
-
-app.get('/appointments',async(req, res)=>{
-const cursor= await appointmentCollection.find()
-const result= await cursor.toArray()
-res.send(result)
-})
+run();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
